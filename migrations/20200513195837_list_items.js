9bfda868fcd1a289796b607ff059364b44d3aab6
mattalagala <matt15y@gmail.com>
2020-05-13 20:02:07 -0500

    Created list_items migration

---
+++ migrations/20200513195837_list_items.js
@@ -0,0 +1,21 @@
+const createListItemsQuery = `
+create table list_items (
+    id serial primary key,
+    uuid text unique,
+    description text,
+    display_order int,
+    list_id int references list(id),
+    ctime timestamptz,
+    mtime timestamptz default CURRENT_TIMESTAMP
+);`
+
+const dropListItemsQuery = `
+drop table list_items;
+`
+exports.up = function(knex) {
+    return knex.raw(createListItemsQuery)  
+};
+
+exports.down = function(knex) {
+    return knew.raw(dropListItemsQuery)
+};
